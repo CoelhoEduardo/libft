45bc9267648d1edba0d035ac401e3a0c013d6928
Eduardo Coelho <eduardo.coelho@javendeu.com>
2023-08-04 12:07:42 -0300

    add ft_split

--- ft_split.c
+++ ft_split.c
@@ -3,15 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecoelho- <ecoelho-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eduardocoelho <eduardocoelho@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 20:13:32 by ecoelho-          #+#    #+#             */
-/*   Updated: 2023/08/03 20:20:23 by ecoelho-         ###   ########.fr       */
+/*   Updated: 2023/08/04 10:01:52 by eduardocoel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	ft_allocate(char **tab, char const *s, char sep)
+{
+	char		**tab_p;
+	char const	*tmp;
+
+	tmp = s;
+	tab_p = tab;
+	while (*tmp)
+	{
+		while (*s == sep)
+			++s;
+		    tmp = s;
+		while (*tmp && *tmp != sep)
+			++tmp;
+		if (*tmp == sep || tmp > s)
+		{
+			*tab_p = ft_substr(s, 0, tmp - s);
+			s = tmp;
+			++tab_p;
+		}
+	}
+	*tab_p = NULL;
+}
+
+static int	ft_count_words(char const *s, char sep)
+{
+	int	word_count;
+
+	word_count = 0;
+	while (*s)
+	{
+		while (*s == sep)
+			++s;
+		if (*s)
+			++word_count;
+		while (*s && *s != sep)
+			++s;
+	}
+	return (word_count);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	char	**new;
+	int		size;
+
+	if (!s)
+		return (NULL);
+	size = ft_count_words(s, c);
+	new = (char **)malloc(sizeof(char *) * (size + 1));
+	if (!new)
+		return (NULL);
+	ft_allocate(new, s, c);
+	return (new);
 }
